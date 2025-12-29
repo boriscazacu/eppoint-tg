b3535bbb63c901f1143c5fe9b6fa7dfe39117590
Boris Cazacu <boris.cazacu@outlook.com>
2025-12-29 12:31:16 +0200

    Remove link

--- assets/scripts/telegram.js
+++ assets/scripts/telegram.js
@@ -6,7 +6,7 @@ export function initTelegram() {
 
     const startParam = tg.initDataUnsafe?.start_param;
     sessionStorage.setItem("start_param", startParam);
-    document.getElementById('tg-start-key').textContent=JSON.stringify(tg.initDataUnsafe)
+    // document.getElementById('tg-start-key').textContent=JSON.stringify(tg.initDataUnsafe)
 
     document.getElementById('username-display').textContent = `${tg.initDataUnsafe.user?.first_name || "Guest"} ${tg.initDataUnsafe.user?.last_name || ""}`.trim();
     sessionStorage.setItem("telegram_key", tg?.initDataUnsafe?.user?.id);
